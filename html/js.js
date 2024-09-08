7ce8387e07a62e0566ce1be35bd2f44927b6f452
Sankhadeep Ghosh <144552017+sankhadeep-ghosh@users.noreply.github.com>
2024-09-08 13:50:11 +0530

    Delete html/js.js

--- html/js.js
+++
@@ -1,8 +0,0 @@
-//constructor function
-function Person (firstName, lastName, dob){
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.dob = new Date(dob);
-}
-const person1 = new Person('Sankhadeep', 'Ghosh', '6-26-2004');
-console.log(person1);
